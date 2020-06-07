3f15f95456fe261631e24474addb614a69bfbdd0
Matthew Leahy <m@ttleahy.com>
2020-06-07 19:16:18 -0400

    Close?

--- next-config.js
+++ next-config.js
@@ -3,6 +3,7 @@
 // (But you could use ES2015 features supported by your Node.js version)
 
 const debug = process.env.NODE_ENV !== 'production'
+
 module.exports = {
   assetPrefix: !debug ? '/mass-dph-data/' : '',
 }
