7f5b274382b5b0e620d6924901479db266b10054
Matthew Leahy <m@ttleahy.com>
2020-06-08 20:44:41 -0400

    Deploy gh-pages

--- _next/static/current/_ssgManifest.js
+++ _next/static/current/_ssgManifest.js
@@ -1,1 +1,1 @@
-self.__SSG_MANIFEST=new Set(["\u002FageOfPatients","\u002FnewHospitalByAge","\u002FnewVsTotal","\u002FpositiveTestRate","\u002FnewCasesByAge","\u002Fchart1","\u002FnewDeathsByAge","\u002FdailyTests","\u002FhospitalizationVsTotal"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set(["\u002FhospitalizationVsTotal","\u002FnewHospitalByAge","\u002FpositiveTestRate","\u002FnewVsTotal","\u002FageOfPatients","\u002FdailyTests","\u002FnewCasesByAge","\u002FnewDeathsByAge","\u002Fchart1"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
