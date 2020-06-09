765ccf0f11db0baf354fa26ae591e0d804abd1e8
Matthew Leahy <m@ttleahy.com>
2020-06-09 16:16:09 -0400

    Cleaning up index

--- pages/index.js
+++ pages/index.js
@@ -21,20 +21,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Mass DPH COVID Data</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Links />
-        <p>Debug={(process.env.NODE_ENV !== 'production') ? "True" : "False"} Post</p>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Mass DPH COVID Data
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
         <div className="grid">
           <PrefixLink href="/chart1">
@@ -43,33 +39,6 @@ export default function Home() {
               <p>The 1st chart I started</p>
             </a>
           </PrefixLink>
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
