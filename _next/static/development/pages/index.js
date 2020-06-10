9fe63ce85053438bf6fa3145451879a191c0333a
Matthew Leahy <m@ttleahy.com>
2020-06-10 19:11:24 -0400

    Deploy gh-pages

--- _next/static/development/pages/index.js
+++ _next/static/development/pages/index.js
@@ -896,7 +896,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* WEBPACK VAR INJECTION */(f
 
 /***/ }),
 
-/***/ 4:
+/***/ 1:
 /*!*****************************************************************************************************************************!*\
   !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2Fhome%2Fmatt%2Fworkspace%2Fmassdphcovid%2Fpages%2Findex.js ***!
   \*****************************************************************************************************************************/
@@ -919,4 +919,4 @@ eval("module.exports = dll_5030f387d328e4415785;//# sourceURL=[module]\n//# sour
 
 /***/ })
 
-},[[4,"static/runtime/webpack.js"]]]);
+},[[1,"static/runtime/webpack.js"]]]);
