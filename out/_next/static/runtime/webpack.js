47cb2040b17fc42ff9d8190f5f2db517fb767366
Matthew Leahy <m@ttleahy.com>
2020-06-28 06:55:03 -0400

    Deploy gh-pages

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4ceb36365013552be19f";
+/******/ 	var hotCurrentHash = "1cb65ad29eb4795167cf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
